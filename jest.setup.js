1fddd7b6393dee1f3e1be7e1cce1ba0321cf9538
Richard <olella93@gmail.com>
2025-04-12 07:07:57 +0300

    test done

---
+++ jest.setup.js
@@ -0,0 +1,10 @@
+jest.setTimeout(10000);
+process.on('exit', () => {
+    const timers = setTimeout(() => {}, 1000);
+    timers.unref();
+});
+
+afterEach(() => {
+    jest.clearAllMocks(); // Clear mocks after each test
+    jest.clearAllTimers(); // Clear timers after each test
+  });
